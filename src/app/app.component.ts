cdbed39391ca1f819dd572aea23ca3c75a9ffd63
prashant-savadi <prsh.jkd@gmail.com>
2017-12-09 18:04:26 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+
+  template : `
+  <h2>::-------------------------------CSS----------------------------------::</h2>
+
+  <h3> This is reday to deploy build!!</h3>
+
+  `,
+  styles: [ `
+h3 {
+  text-decoration: underline;
+}
+
+h2, h3 {
+  color:#00ad7d;
+}
+`],
+
 })
 export class AppComponent {
   title = 'app';
+
+
 }
